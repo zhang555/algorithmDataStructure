9cd40390b5c8f3e35a25722ff70b0dc6aa8516c0
zhang555 <4598181@qq.com>
2020-04-14 21:58:04 +0800

    new

--- algorithm/memory/clock/clock1/clock.go
+++ algorithm/memory/clock/clock1/clock.go
@@ -1,5 +1,13 @@
 package clock1
 
+/*
+时钟置换算法
+每一个页框关联一个附加位， 首次装入内存时，附加位为1 ， 如果被访问到， 置1 。
+
+需要替换时，查找isUse为0的， 如果遇到isUse为1的置0，然后继续查找下一个，
+如果查找到了最后一个页框，从最后一个页框跳转到第一个页框 进行查找
+如果全部查找一遍 没有isUse为0的页框，就替换当前页框。
+*/
 type Clock struct {
 	m        map[int]int
 	list     []KV
@@ -48,8 +56,11 @@ func (this *Clock) Put(key int, value int) {
 			return
 		}
 
+		//找到一个需要丢弃的页框
 		if len(this.m) == this.cap {
 			for i := 0; i < this.cap; i++ {
+
+				//IsUse == false 替换
 				if !this.list[this.position].IsUse {
 					delete(this.m, this.list[this.position].Key)
 					this.m[key] = this.position
@@ -86,6 +97,7 @@ func (this *Clock) Put(key int, value int) {
 	}
 }
 
+//PositionIncrease 页框位置加一，如果超出，就置0
 func (this *Clock) PositionIncrease() {
 	this.position++
 	if this.position >= this.cap {
